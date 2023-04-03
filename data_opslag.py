b50b2a7325d7b7e414b115b6ede3d7585d199e08
Stijn K <kerss@hr.nl>
2023-04-03 14:07:36 +0200

    Data opslag toegevoegd

--- data_opslag.py
+++ data_opslag.py
@@ -1,15 +1,26 @@
-bestandsnaam = "voorbeeld_csv.txt"
+from extras import DataRecoder
 
-# 'w' = write (schrijven nieuw bestand)
-with open(bestandsnaam, 'w') as file:
-    regel_tekst = 'tijd, temperatuur, luchtvochtigheid\n'
-    file.write(regel_tekst)
+
+data_recorder = DataRecoder('test_tijds_les.txt', ['tijd', 'temperatuur', 'luchtvochtigheid'])
 
 tijd = 0.1
 meetwaarde1 = 240.231
 meetwaarde2 = 2
 
-# 'a' = appenden (toevoegen)
-with open(bestandsnaam, 'a') as file:
-    regel_tekst = str(tijd) + ',' + str(meetwaarde1) + ',' + str(meetwaarde2) + '\n'
-    file.write(regel_tekst)
+data_recorder.record([tijd, meetwaarde1, meetwaarde2])
+
+# bestandsnaam = "voorbeeld_csv.txt"
+
+# # 'w' = write (schrijven nieuw bestand)
+# with open(bestandsnaam, 'w') as file:
+#     regel_tekst = 'tijd, temperatuur, luchtvochtigheid\n'
+#     file.write(regel_tekst)
+
+# tijd = 0.1
+# meetwaarde1 = 240.231
+# meetwaarde2 = 2
+
+# # 'a' = appenden (toevoegen)
+# with open(bestandsnaam, 'a') as file:
+#     regel_tekst = str(tijd) + ',' + str(meetwaarde1) + ',' + str(meetwaarde2) + '\n'
+#     file.write(regel_tekst)
--- extras.py
+++ extras.py
@@ -1,4 +1,10 @@
 from machine import Pin, ADC
+import os
+
+try:
+    from typing import List, Any
+except ImportError:
+    pass
 
 
 class Button:
@@ -26,3 +32,26 @@ class Button:
             # Doe niets
                         
             pass
+
+
+class DataRecoder:
+    def __init__(self, bestandsnaam: str, recorder_titels: List[str]) -> None:
+        try: 
+            os.listdir('recorded_data/')
+        except OSError:
+            os.mkdir('recorded_data')
+
+        teller = 1
+        for file in os.listdir('recorded_data/'):
+            if bestandsnaam in file:
+                teller += 1
+
+        self.bestandsnaam = f'recorded_data/{bestandsnaam}_{teller:03}.txt'
+
+        with open(self.bestandsnaam, 'w') as file:
+            eerste_regel = str(recorder_titels)[1:-1] + '\n'
+            file.write(eerste_regel)
+
+    def record(self, recorder_values: List[Any]):
+        with open(self.bestandsnaam, 'a') as file:
+            file.write(str(recorder_values)[1:-1] + '\n')
